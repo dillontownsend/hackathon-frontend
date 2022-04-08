6d101f7e946c8a8776906fe95835f1fa686024a2
Dillon Townsend <townsenddillon.dt@gmail.com>
2022-04-07 20:45:31 -0500

    init commit

---
+++ src/components/BlueButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const BlueButton = () => {
+  return (
+    <>
+        <Button variant='primary'>Test button</Button>
+    </>
+  )
+}
+
+export default BlueButton

b7e9b17290ff7cf680f87b9d66ab3d207d3efc7c
Dillon Townsend <townsenddillon.dt@gmail.com>
2022-04-09 00:46:14 -0500

    landing page started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Register from './pages/Register';
-import Login from './pages/Login';
+import Register from './pages/Register'
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import LandingPage from './pages/LandingPage'
 
 function App() {
     return (
         <Router>
-        <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-    </Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
     )
 }
 
